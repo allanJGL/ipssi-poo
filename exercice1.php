bf4f68822eeeac7c2a4f0118222e72a5213ff9a1
Allan Jaegly <allan.jaegly@goldenbees.fr>
2019-11-15 10:01:43 +0100

    exo1

--- exercice1.php
+++ exercice1.php
@@ -2,9 +2,14 @@
 
 require_once('vendor/autoload.php');
 
-$climate = new League\CLImate\CLImate;
+use Ipssi\Evaluation\InvalidIndexException;
+use Ipssi\Evaluation\InvalidParametersException;
 
-class Diviseur {
+$climate = new League\CLImate\CLImate;
+$endProcess = false;
+
+class Diviseur
+{
     public function division(int $index, int $diviseur)
     {
         $valeurs = [17, 12, 15, 38, 29, 157, 89, -22, 0, 5];
@@ -13,11 +18,25 @@ class Diviseur {
     }
 }
 
-$input = $climate->input("Entrez l’indice de l’entier à diviser : ");
-$index = $input->prompt();
+do {
+    $input = $climate->input("Entrez l’indice de l’entier à diviser : ");
+    $index = $input->prompt();
 
-$input = $climate->input("Entrez le diviseur : ");
-$diviseur = $input->prompt();
+    $input = $climate->input("Entrez le diviseur : ");
+    $diviseur = $input->prompt();
 
-$climate->output("Le résultat de la division est : " . (new Diviseur())->division($index, $diviseur));
-
+    try {
+        if (!is_numeric($index) || !is_numeric($diviseur)) {
+            throw new InvalidParametersException(array($index, $diviseur));
+        }
+        if ($index > 9) {
+            throw new InvalidIndexException($index);
+        }
+        $climate->output("Le résultat de la division est : " . (new Diviseur())->division($index, $diviseur));
+        $endProcess = true;
+    } catch (InvalidParametersException $e) {
+        echo $e->getMessage();
+    } catch (InvalidIndexException $e) {
+        echo $e->getMessage();
+    }
+} while ($endProcess !== true);
---
+++ src/InvalidParametersException.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Ipssi\Evaluation;
+
+
+class InvalidParametersException extends InvalidDataException
+{
+    public function __construct($invalidData)
+    {
+        $message = $invalidData[0] . " and " . $invalidData[1] . "' given." . PHP_EOL . "Please enter numbers only";
+            parent::__construct($invalidData, "parameters", \Diviseur::class, $message);
+    }
+}
